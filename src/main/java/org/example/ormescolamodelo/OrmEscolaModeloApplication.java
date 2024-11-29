668cdfa7db8ccc806c94e085a20f4030cadceb7e
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-29 19:56:46 -0300

    Initial commit

---
+++ src/main/java/org/example/ormescolamodelo/OrmEscolaModeloApplication.java
@@ -0,0 +1,13 @@
+package org.example.ormescolamodelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmEscolaModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmEscolaModeloApplication.class, args);
+	}
+
+}
