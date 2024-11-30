052e76b053fc293a0deb82b37422e620d223fc33
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-29 22:14:43 -0300

    Update OrmEscolaModeloApplication.java

--- src/main/java/org/example/ormescolamodelo/OrmEscolaModeloApplication.java
+++ src/main/java/org/example/ormescolamodelo/OrmEscolaModeloApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class OrmEscolaModeloApplication implements CommandLineRunner {
@@ -42,11 +43,44 @@ public class OrmEscolaModeloApplication implements CommandLineRunner {
 	@Override
 	@Transactional
 	public void run(String... args) throws Exception {
+//		Aluno aluno = new Aluno();
+//		aluno.setNome("Lucas Juszczak");
+//		aluno.setNascimento(LocalDate.of(2002,5,12));
+//		aluno.setEndereco("Logo Ali");
+//		aluno.setNumero_matricula("2024001");
+//		aluno.setCpf("12345678901");
+//		alunoRepo.save(aluno);
+
+//		Contato contato = new Contato();
+//		contato.setDescricao("contato@email.com");
+//		contato.setTipo("E-mail");
+//		contatoRepo.save(contato);
+//
+//		Curso curso = new Curso();
+//		curso.setNome("Tecnologia em Análise e Desenvolvimento de Sistemas");
+//		curso.setCoordenador("Douglas");
+//		cursoRepo.save(curso);
+//
+//		Disciplina disciplina = new Disciplina();
+//		disciplina.setNome("Programação Orientada a Objetos II");
+//		disciplinaRepo.save(disciplina);
+//
 //		Professor professor = new Professor();
 //		professor.setNome("Tiago Roberto Kautzmann");
 //		professorRepo.save(professor);
-
-		
+//
+//		Responsavel responsavel = new Responsavel();
+//		responsavel.setNome("Alguém Ali");
+//		responsavel.setEndereco("Em Tal Lugar");
+//		responsavelRepo.save(responsavel);
+//
+//		Turma turma = new Turma();
+//		turma.setSemestre("4º Período");
+//		turmaRepo.save(turma);
+//
+//		Usuario usuario = new Usuario();
+//		usuario.setUsuario("Lucas");
+//		usuario.setSenha("123456");
+//		usuarioRepo.save(usuario);
 	}
-
 }
